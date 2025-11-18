3239255629d592544cfea2455646aaf7551dd1a1
Fernando Lopez <162709429+fernando-lopez-codex@users.noreply.github.com>
2025-11-18 02:27:47 +0000

    Formula fixed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function monthlyPayment(){
 
     let P = document.getElementById("amount").value;
-    let r= document.getElementById("interest").value;
+    let r= (document.getElementById("interest").value / 100) /12  ;
     let n = document.getElementById("months").value;
 
-    let payment= P * (r * Math.pow((1+r/100),n)) /(Math.pow((1+r/100),n) -1);
+    let payment= P * ( r * Math.pow((1+r),n) ) / (Math.pow((1+r),n)-1) ;
 
     let result = document.getElementById("result");
-    result.textContent = `The monthly payment would be ${payment}`;
+    result.textContent = `The monthly payment would be $${payment.toFixed(2)}`;
     
 }
