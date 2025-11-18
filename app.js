d3b03cbea7a33e79f24fa6a1d1ffb03e9e5cc62c
Fernando Lopez <162709429+fernando-lopez-codex@users.noreply.github.com>
2025-11-18 02:04:49 +0000

    Function definition

---
+++ app.js
@@ -0,0 +1,11 @@
+function monthlyPayment(){
+
+    let P = document.getElementById("amount").value;
+    let r= document.getElementById("interest").value;
+    let n = document.getElementById("months").value;
+
+    alert(`The values are P = ${P} - r =${r} - n = ${n}`)
+
+    let payment=0;
+    
+}
